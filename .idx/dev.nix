a065835f97a36f20d624be6c48eb80a63723523c
Nahuel Borja <nahuelemilianoborja33@gmail.com>
2026-03-31 17:09:46 +0000

    Agregado ejercicio 1

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -6,9 +6,10 @@
   # Use https://search.nixos.org/packages to find packages
   packages = [
     # pkgs.go
+    pkgs.openssh
     # pkgs.python311
     # pkgs.python311Packages.pip
-    # pkgs.nodejs_22
+    pkgs.nodejs_22
     # pkgs.nodePackages.nodemon
   ];
   # Sets environment variables in the workspace
